0c8b065b49295e086f45dc179117a602aff39188
Nick Rimer <nickrimer.03@gmail.com>
2020-05-28 12:14:27 +0300

    -- .env fix in prod mode

---
+++ app.mjs
@@ -0,0 +1,21 @@
+import Discord from "discord.js";
+import emitter from "./src/emitter.mjs";
+import AppState from "./src/state.mjs";
+import { log } from "./src/utils.mjs";
+import { botReady } from "./src/texts.mjs";
+
+export default () => {
+  const state = new AppState();
+  const client = new Discord.Client();
+
+  client.on("ready", () => {
+    log(botReady);
+    state.bot = true;
+  });
+
+  client.on("message", (message) => {
+    emitter.emit("onmessage", { message, state, client });
+  });
+
+  client.login(process.env.BOT_TOKEN).catch((err) => console.log(err));
+};
--- core.mjs
+++ core.mjs
@@ -1,23 +1,17 @@
-import path from "path";
-import dotEnv from "dotenv";
-import Discord from "discord.js";
-import emitter from "./src/emitter.mjs";
-import AppState from "./src/state.mjs";
-import { log } from "./src/utils.mjs";
-import { botReady } from "./src/texts.mjs";
+import app from "./app.mjs";
 
-dotEnv.config({ path: path.resolve() + "/.env" });
-
-const state = new AppState();
-const client = new Discord.Client();
-
-client.on("ready", () => {
-  log(botReady);
-  state.bot = true;
-});
-
-client.on("message", (message) => {
-  emitter.emit("onmessage", { message, state, client });
-});
-
-client.login(process.env.BOT_TOKEN).catch((err) => console.log(err));
+if (process.env.BUILD_MODE !== "production") {
+  Promise.all([import("dotenv"), import("path")]).then(
+    ([
+      {
+        default: { config },
+      },
+      path,
+    ]) => {
+      config({ path: path.resolve() + "/.env" });
+      app();
+    }
+  );
+} else {
+  app();
+}
